75bef7bd42f1a52acb3b1131cf8a875858468be0
Daria Bondarenko <mell_hime@mail.ru>
2025-04-22 10:35:18 +0300

    review fixes

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -12,8 +14,15 @@ export default defineConfig([
   },
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    plugins: { js },
+    plugins: {
+      js,
+      prettier: pluginPrettier,
+    },
     extends: ["js/recommended"],
+    rules: {
+      ...eslintConfigPrettier.rules,
+      "prettier/prettier": "error",
+    },
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
